7c46c61994758e6788b2bcacad27277611f0efe7
Bruno Martins <bgcngm@gmail.com>
2017-12-22 14:43:24 +0000

    santoni: Bring config.fs inline with CAF
     * As per Treble, device-specific stuff is placed on vendor,
       so a follow-up change will take care of moving the binaries
       referenced here to the proper place.
    
     * Legacy AID's have been kept as we are still using pre-O blobs
       (to be removed once we get proper Oreo blobs).
    
     * This was based on tag LA.UM.6.5.r1-04300-8x96.0.
    
    Change-Id: Ieb85f990825148b9e58a5e76c261dae6fd8ef142

--- config.fs
+++ config.fs
@@ -1,6 +1,9 @@
 [AID_QCOM_DIAG]
 value:2950
 
+[AID_QTI_DIAG]
+value:2901
+
 [AID_RFS]
 value:2951
 
@@ -96,6 +99,18 @@ mode: 0755
 user: AID_SYSTEM
 group: AID_RADIO
 caps: NET_BIND_SERVICE
+
+[vendor/bin/imsrcsd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: WAKE_ALARM
+
+[system/vendor/bin/imsrcsd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: WAKE_ALARM
 
 [vendor/bin/cnd]
 mode: 0755
