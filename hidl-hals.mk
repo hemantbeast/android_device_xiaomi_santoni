6807362986858b04f13296aae5ad9217a3c6ce97
dianlujitao <dianlujitao@lineageos.org>
2018-01-19 21:13:47 +0530

    santoni: Build dummy android.hidl.[base,manager]@1.0
     * Required by Oreo-MR0 blobs
    
    Change-Id: I81a081130869c4d92f30b2674e75bb6b096a7a2c

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -77,6 +77,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl
