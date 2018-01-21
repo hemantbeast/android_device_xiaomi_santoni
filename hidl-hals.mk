599403b4ef68464076db6b789dce7859fcfc8986
Hemant Sharma <hemantbeast@gmail.com>
2018-01-21 19:11:08 +0530

    santoni: Update camera hidl hals

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -28,10 +28,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl \
-    vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor
+    camera.device@3.2-impl
 
 # Configstore
 PRODUCT_PACKAGES += \
