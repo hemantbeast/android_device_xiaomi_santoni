9d825f8674ee05872bf7c64f0265fdc8cfd431b5
Bruno Martins <bgcngm@gmail.com>
2018-01-25 21:32:35 +0530

    santoni: Build QTI camera HIDL extension
     * This is a dependency for IMS blobs
    
    Change-Id: I58a787e4b4109a643ba64fca0254b347be6e66a2

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -28,7 +28,9 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    camera.device@3.2-impl
+    camera.device@3.2-impl \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor
 
 # Configstore
 PRODUCT_PACKAGES += \
